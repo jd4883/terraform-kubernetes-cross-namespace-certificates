765f917b3e3c2d5ff0f405390ffc41e56172c370
Condoriano <15719920+jd4883@users.noreply.github.com>
2021-12-18 06:34:08 +0000

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -2,6 +2,7 @@ terraform {
   required_providers {
     local      = { source = "hashicorp/local" }
     helm       = { source = "hashicorp/helm" }
+    kubectl    = { source = "gavinbunney/kubectl" }
     kubernetes = { source = "hashicorp/kubernetes" }
   }
 }
